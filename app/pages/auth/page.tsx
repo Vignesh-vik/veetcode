c4bc332567931af05a34a76c80d4222946f1d37a
Vignesh S <vignesh1255hg@gmail.com>
2025-12-19 13:51:55 +0530

    added hero

--- app/pages/auth/page.tsx
+++ app/pages/auth/page.tsx
@@ -1,15 +1,17 @@
-import NavBar from '@/app/components/Navbar'
-import React from 'react'
+import NavBar from "@/app/components/Navbar";
+import React from "react";
 
 const AuthPage = () => {
   return (
-    <div className='bg-linear-to-b from-gray-400 to-black h-screen relative'>
-      <div className='max-w-7xl mx-auto'>
-        <NavBar/>
-        
+    <div className="bg-linear-to-b from-gray-400 to-black h-screen relative">
+      <div className="max-w-7xl mx-auto">
+        <NavBar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <img src="/hero.png" alt="Hero img" width={700} height={700} />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthPage
+export default AuthPage;
