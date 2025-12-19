516d6c2c2d7c8dcb7c4d4593a2336face9a4a350
Vignesh S <vignesh1255hg@gmail.com>
2025-12-19 13:48:08 +0530

    initialized the project and added the auth routing and navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+// import { authModalState } from "@/atoms/authModalAtom";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+// import { useSetRecoilState } from "recoil";
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+	
+	return (
+		<div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
+			<Link href='/' className='flex items-center justify-center h-20'>
+				<Image src='/logo.png' alt='LeetClone' height={200} width={200} />
+			</Link>
+			<div className='flex items-center'>
+				<button
+					className=' bg-orange-400 text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+                hover:text-orange-400 hover:bg-white hover:border-2 hover:border-orange-400 border-2 border-transparent
+                transition duration-300 ease-in-out
+                '
+					// onClick={handleClick}
+				>
+					Sign In
+				</button>
+			</div>
+		</div>
+	);
+};
+export default Navbar;
---
+++ app/pages/auth/page.tsx
@@ -0,0 +1,15 @@
+import NavBar from '@/app/components/Navbar'
+import React from 'react'
+
+const AuthPage = () => {
+  return (
+    <div className='bg-linear-to-b from-gray-400 to-black h-screen relative'>
+      <div className='max-w-7xl mx-auto'>
+        <NavBar/>
+        
+      </div>
+    </div>
+  )
+}
+
+export default AuthPage
